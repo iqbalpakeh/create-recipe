abdb1cfd71c11caeb1e7b8706b7920326b24edcb
Progrema Studio <iqbalpakeh@gmail.com>
2020-06-20 07:16:01 +0700

    Initial files

---
+++ src/test/java/com/progrema/createrecipe/CreateRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progrema.createrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
